0f716ca689a17d5f78a8c1902bc06712ccbcc274
Ray Minge <ray@rayminge.com>
2015-11-07 00:06:04 -0600

    Moving some things around

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
                // if the source file has an extension of es6 then
                // we change the name of the source file accordingly.
                // The result file's extension is always .js
-               "./dist/module.js": ["./modules/index.es6"]
+               "./dist/module.js": ["./modules/Hadron.es6"]
             }
          }
       },
---
+++ modules/Hadron.es6
@@ -0,0 +1,8 @@
+"use strict";
+
+import { Event } from "./core/Event.es6";
+import { DataSet } from "./core/DataSet.es6";
+
+var Hadron = { Event, DataSet };
+
+global.Hadron = Hadron;
